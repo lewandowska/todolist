6ba099cfe99cfd9a6bd563c3a8fadb354436d7e5
Magdalena Lewandowska <magdalenalewandowska@MacBook-Pro-Magdalena.local>
2018-06-26 23:08:17 +0200

    przebudowa/animacja/gsap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 window.onload = function () {
 
-"use strict";
+    const tl = new TimelineLite({delay: 0.5});
+    const greenLeaf = document.querySelectorAll('.green-leaf');
+    const yellowLeaf = document.querySelectorAll('.yellow-leaf');
+    const crab = document.querySelectorAll('.crab');
+    const crabLeg1 = document.querySelectorAll('#leg1');
+    const crabLeg2 = document.querySelectorAll('#leg4');
+
+    tl.add([
+        TweenMax.to(greenLeaf, 4, {x: 20, rotation: "10", transformOrigin:"center center",yoyo: true, repeatDelay: 1, repeat: -1}),
+        TweenMax.to(yellowLeaf, 3, {rotation: "-40", transformOrigin:"center center",yoyo: true, repeatDelay:0, repeat: -1}),
+        TweenMax.set(crab, { x: -250,zIndex: -5} ),
+        TweenMax.to(crab, 50, { x: 1100}),
+        TweenMax.to(crab, 50, { y: -620}, 6),
+        TweenMax.to(crabLeg1, 0.5, { y: 5,repeat: -1, yoyo:true}),
+        TweenMax.to(crabLeg2, 0.5, { y: -5,repeat: -1, yoyo:true})
+    ]);
 
     new Vue({
         
@@ -9,9 +24,7 @@ window.onload = function () {
         data: {
             newTask: "",
             tasks: [
-                {
-                    text: "Learn Vue.js course"
-                }
+             
             ],
 
         },
@@ -29,7 +42,7 @@ window.onload = function () {
         methods: {
 
             addTask: function () {
-                var task = this.newTask.trim();
+                const task = this.newTask.trim();
                 if (task) {
                     this.tasks.push({text: task});
                     this.newTask = "";
